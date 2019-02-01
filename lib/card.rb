f62130d86ad564ac71ab6cc3fa8bd41f63dce37f
f1nesse13 <philly.teg@gmail.com>
2019-01-31 23:51:27 -0500

    added tests/methods for pairs, ToK and straight

--- lib/card.rb
+++ lib/card.rb
@@ -33,32 +33,25 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    @card_value = find_value(@value)
   end
 
   def to_s
     "|#{CARD_VALUES[value]}#{CARD_SUITS[suit]}|"
   end
 
-  private
+  def ==(next_card)
+    (self.value == next_card.value) &&
+    (self.suit == next_card.suit)
+  end
 
-  def find_value(value)
-    card_values = {
-      :two => 2,
-      :three => 3,
-      :four => 4,
-      :five => 5,
-      :six => 6,
-      :seven => 7,
-      :eight => 8,
-      :nine => 9,
-      :ten => 10,
-      :jack => 11,
-      :queen => 12,
-      :king => 13,
-      :ace => 14
-    }
-    card_values[@value]
+  def <=>(next_card)
+    if self == next_card
+      0
+    elsif self.value != next_card.value
+      Card.values.index(value) <=> Card.values.index(next_card.value)
+    elsif self.suit != next_card.suit
+      Card.suits.index(suit) <=> Card.suits.index(next_card.suit)
+    end
   end
 
 end
--- lib/hand.rb
+++ lib/hand.rb
@@ -1,16 +1,40 @@
-require_relative 'deck'
-
+require_relative 'card'
 class Hand
-attr_accessor :cards
+
+  POSSIBLE_HANDS = {
+    :royal_flush => 27,
+    :straight_flush => 26,
+    :four_of_a_kind => 25,
+    :full_house => 24,
+    :flush => 23,
+    :straight => 22,
+    :three_of_a_kind => 21,
+    :two_pair => 21,
+    :one_pair => 20
+  }
+  attr_accessor :cards
   def initialize(cards)
-    @cards = cards.sort
+    raise 'must contain 5 cards' unless cards.count == 5
+    @cards = cards
+    @hand_value = nil
+    @matches = []
+    @card_values = []
+    @cards.sort!
+  end
+
+  def to_s
+    @cards.join(" ")
+  end
+
+  def sort!
+    @cards.sort!
   end
 
   def exchange_cards(old_cards, new_cards)
     raise 'hand must contain 5 cards' if old_cards.count != new_cards.count
     discard_old_cards(old_cards)
     take_new_cards(new_cards)
-    @cards.sort!
+    old_cards
   end
 
   def take_new_cards(cards)
@@ -21,8 +45,48 @@ attr_accessor :cards
     old_cards.each { |card| @cards.delete(card) }
   end
 
-  def sort!
-    @cards.sort!
+  def value_count(value)
+    @cards.map(&:value).count(value)
   end
 
+  def has_a?(value)
+    @cards.map(&:value).include?(value) ||
+    @cards.map(&:suit).include?(value)
+  end
+
+  def pairs
+    pairs = []
+    @cards.map(&:value).uniq.each do |val|
+      if value_count(val) == 2
+        pairs << @cards.select {|card| card.value == val} 
+      end
+    end
+    pairs
+  end
+  
+  def one_pairs
+    pairs.count == 1
+  end
+
+  def two_pairs
+    pairs.count == 2
+  end
+  
+  def three_of_a_kind
+    @cards.map(&:value).each do |val|
+      return value_count(val) == 3
+    end
+  end
+
+  def straight
+    if @cards.has_a?(:ace) && @cards.has_a?(:two)
+      straight = Card.values[0..3] + :ace
+    else
+      start_pos = Card.values.index(@cards[0].value)
+      straight = Card.values[start_pos..(start_pos + 4)] 
+    end
+    @cards.map(&:value) == straight 
+    end
+
 end
+
